d7d3069a3c6a0035f485fc5818ec205f170f4394
John Doe <johndoe@example.com>
2020-08-20 14:04:13 +0900

    middleware: call action async

--- src/js/client.js
+++ src/js/client.js
@@ -1,42 +1,16 @@
 import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INC":
-      state = state + 1;
-      break;
-    case "DEC":
-      state = state - 1;
-      break;
-    case "ERR":
-      throw new Error("It'S error!!!");
-  }
+const reducer = (state = {}, action) => {
   return state;
-}
+};
 
-const logger = (store) => (next) => (action) => {
-  console.log("action fiedl", action);
-  next(action);
-}
+const middleware = applyMiddleware(thunk, createLogger());
+const store = createStore(reducer, middleware);
 
-const error = (store) => (next) => (action) => {
-  try {
-    next(action);
-  } catch (e) {
-    console.log("Error was occured", e);
-  }
-}
-
-const middleware = applyMiddleware(logger, error);
-
-const store = createStore(reducer, 1, middleware);
-
-store.subscribe(() => {
-  console.log("store changed", store.getState());
+store.dispatch((dispatch) => {
+  dispatch({ type: "FOO" });
+  // async
+  dispatch({ type: "BAR" });
 });
-
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "DEC" });
-store.dispatch({ type: "DEC" });
-store.dispatch({ type: "ERR" });
