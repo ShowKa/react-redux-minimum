7741ab4b0b9e29a508b0df5115c3fe61d250aa26
John Doe <johndoe@example.com>
2020-08-20 11:45:46 +0900

    redux : hello world

---
+++ src/js/client.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case "INC":
+      return state + action.payload;
+      case "DEC":
+        return state - action.payload;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 100});
+store.dispatch({type: "DEC", payload: 50});
